c1eba314a589c22c0f13631e79d021ebd2da24b5
panychev_dima <splitrarrys@gmail.com>
2024-10-28 23:27:59 +0300

    исправление ошибок

--- еtask 2.1/еtask 2.1.c
+++ еtask 2.1/еtask 2.1.c
@@ -1,20 +1,67 @@
-﻿// еtask 2.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#define _USE_MATH_DEFINES
 
-#include <iostream>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
 
-int main()
+/**
+@brief проверяет вводимое значение, является ли оно числом
+@return Возвращает введённое значение, если оно является числом
+*/
+double input(void);
+
+/**
+@brief Функция для нахождения площади трапеции
+@param upper_base Параметр верхнее основание трапеции
+@param lower_base Параметр нижнее основание трапеции
+@param height Параметр высота трапеции
+@return Возвращает значение площади трапеции
+*/
+double area_of_a_trapezoid(const double upper_base, const double lower_base, const double height);
+
+/**
+@brief Функция для нахождения площади круга
+@param  radius Параметр радиус
+@return Возвращает значение площади трапеции
+*/
+double area_of_circle(const double radius);
+
+/**
+@brief Точка входа в программу
+@return Возвращает в случае успеха
+*/
+int main(void)
 {
-    std::cout << "Hello World!\n";
+    printf("Trapezoid:\nPrint upper base\t");
+    const double upper_base = input();
+    printf("Print lower base\t");
+    const double lower_base = input();
+    printf("Print height\t");
+    const double height = input();
+
+    printf("Circle:\nPrint radius\t");
+    const double radius = input();
+
+
+    printf("Area of a trapezoid:\t%lf\nArea of a circle :\t%lf", area_of_a_trapezoid(upper_base, lower_base, height),23area_of_circle(radius));
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+double input(void)
+{
+    double number = 0.0;
+    if (scanf_s("%lf", &number) != 1) {
+        printf("input error");
+        exit(EXIT_FAILURE);
+    }
+    return number;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+double area_of_a_trapezoid(const double a, const double b, const double h)
+{
+    return (a + b) * h / 2;
+}
+
+double area_of_circle(const double radius)
+{
+    return pow(radius, 2) * M_PI;
+}
