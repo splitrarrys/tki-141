e8cbd8ef1845598848d9f670773661bc736d8865
panychev_dima <splitrarrys@gmail.com>
2024-10-28 12:20:21 +0300

    исправление задания 1.2

--- task1.2/task1.2.c
+++ task1.2/task1.2.c
@@ -1,41 +1,54 @@
-﻿#include <stdio.h>
+﻿#define _USE_MATH_DEFINES // for C
+#include <stdio.h>
 #include <math.h>
-#define M_PI (3.14159265358979323846)
+#include <stdlib.h>
 
 /**
 @brief функция для вычесления объёма шара
 @param R значение радиуса шара
 @return возвращает значение объёма шара
 */
-float func_V_ball(float R);
+double get_volume(const double radius);
 
 /**
 @brief функция для вычесления площади поверхноси шара
 @param R значение радиуса шара
 @return возвращает значение площади повехности шара
 */
-float func_S_ball(float R);
+double get_surface(const double R);
+
+/**
+* @brief проверяет вводимое значение (является ли числом)
+* @return возвращает введённое значение если оно является числом
+*/
+double input(void);
 
 /**
  * @brief Точка входа в программу.
  * @return Возвращает в случае успеха.
 */
 int main() {
-    float R;
     printf("Enter R:\t");
-    scanf_s("%f", &R);
-    float V = func_V_ball(R);
-    float S = func_S_ball(R);
-    printf("Volume of a ball:%f\nLateral surface area of a ball:%f", V, S);
+    double R = input();
+    printf("Volume of a ball:%f\nLateral surface area of a ball:%f", get_volume(R), get_surface(R));
     return 0;
 }
 
-float func_V_ball(float R) {
+double get_volume(const double R) {
     float V = R * R * R * 4 / 3 * M_PI;
     return V;
 }
 
-float func_S_ball(float R) {
+double get_surface(const double R) {
     float S = 4 * R * R * M_PI;
     return S;
+}
+
+double input(void) {
+    double number;
+    if (scanf_s("%lf", &number) != 1) {
+        printf("Input error");
+        exit(EXIT_FAILURE);
+    }
+    return number;
 }
