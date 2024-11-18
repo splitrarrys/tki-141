c6ade1dd4d72221f0e934e3a954b16656e8e6a2d
panychev_dima <splitrarrys@gmail.com>
2024-11-18 14:53:15 +0300

    исправление

--- task 2.3/task 2.3.c
+++ task 2.3/task 2.3.c
@@ -13,39 +13,17 @@ double input(void);
 @return Возвращает в случае успеха
 */
 int main() {
-    double result1 = 0, result2 = 0, result3 = 0;
-
     for (int i = 0; i < 3; ++i) {
         double number = input();
+
         if (number > 0) {
-            if (i == 0) result1 = number * number;
-            else if (i == 1) result2 = number * number;
-            else if (i == 2) result3 = number * number;
+            printf("%lf\n", number * number);
         }
         else if (number < 0) {
-            if (i == 0) result1 = pow(number, 4);
-            else if (i == 1) result2 = pow(number, 4);
-            else if (i == 2) result3 = pow(number, 4);
+            printf("%lf\n", pow(number, 4));
         }
         else {
-            if (i == 0) result1 = 0;
-            else if (i == 1) result2 = 0;
-            else if (i == 2) result3 = 0;
-        }
-    }
-
-    for (int i = 0; i < 3; ++i) {
-        if (i == 0) {
-            if ((result1 == 0)) printf("input error");
-            else printf("%lfn", result1);
-        }
-        else if (i == 1) {
-            if ((result2 == 0)) printf("input error");
-            else printf("%lfn", result2);
-        }
-        else if (i == 2) {
-            if ((result3 == 0)) printf("input error");
-            else printf("%lfn", result3);
+            printf("input error\n");
         }
     }
 
@@ -55,7 +33,7 @@ int main() {
 double input(void) {
     double number = 0;
     if (scanf_s("%lf", &number) != 1) {
-        printf("input error");
+        printf("input error\n");
         exit(EXIT_FAILURE);
     }
     return number;
